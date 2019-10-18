3cc375dafda8047a41c92612e34d1e97a843e2ba
Rachelle Perez <rachelleaperez@gmail.com>
2019-10-18 10:12:29 -0400

    adds alternative

--- sql.sql
+++ sql.sql
@@ -40,10 +40,10 @@
  --   if movie PG-13 and rent price is > 1 AND <= 2 THEN new price 1
  --   else movie PG 13 new price = rent price (current)
 
---Assignment Output: Updated Price list for PG-13 movies >> film_id, title, current_rental_rate, new_price
-
 
 *** QUERY ***
+
+-- Assignment Output: Updated Price list for PG-13 movies >> film_id, title, current_rental_rate, new_price
 
 WITH pg13_films AS (
     SELECT film_id, title, rental_rate
@@ -63,34 +63,68 @@ SELECT
 FROM pg13_films
 ORDER BY film_id;
 
-**** ANSWER (FIRST ROWS ONLY!) ***
 
- film_id |            title            | current_rental_rate | promo_rental_rate
----------+-----------------------------+---------------------+-------------------
-       7 | Airplane Sierra             |                4.99 |              4.99
-       9 | Alabama Devil               |                2.99 |              2.99
-      18 | Alter Victory               |                0.99 |              0.10
-      28 | Anthem Luke                 |                4.99 |              4.99
-      33 | Apollo Teen                 |                2.99 |              2.99
-      35 | Arachnophobia Rollercoaster |                2.99 |              2.99
-      36 | Argonauts Town              |                0.99 |              0.10
-      44 | Attacks Hate                |                4.99 |              4.99
-      45 | Attraction Newton           |                4.99 |              4.99
-      48 | Backlash Undefeated         |                4.99 |              4.99
-      57 | Basic Easy                  |                2.99 |              2.99
-      64 | Beethoven Exorcist          |                0.99 |              0.10
-      67 | Berets Agent                |                2.99 |              2.99
-      71 | Bilko Anonymous             |                4.99 |              4.99
-      73 | Bingo Talented              |                2.99 |              2.99
-      79 | Blade Polish                |                0.99 |              0.10
-      81 | Blindness Gun               |                4.99 |              4.99
-      94 | Braveheart Human            |                2.99 |              2.99
-      96 | Breaking Home               |                2.99 |              2.99
-      98 | Bright Encounters           |                4.99 |              4.99
-     108 | Butch Panther               |                0.99 |              0.10
-     124 | Casper Dragonfly            |                4.99 |              4.99
-     130 | Celebrity Horn              |                0.99 |              0.10
-     141 | Chicago North               |                4.99 |              4.99
-     152 | Circus Youth                |                2.99 |              2.99
-     155 | Cleopatra Devil             |                0.99 |              0.10
-     157 | Clockwork Paradise          |                0.99 |              0.10
+*** ALTERNATIVE *** (ALL MOVIES WITH NEW PRICES, CHANGE ONLY ON PG-13 rental_price <2)
+
+-- Assigment Output: Updated price list for all films during promo time
+
+** VERSION 1**
+
+SELECT 
+    film_id, 
+    title,
+    rating,
+    rental_rate AS current_rental_rate, 
+    CASE
+            WHEN (rating = 'PG-13' AND rental_rate <= 1) THEN 0.10
+            WHEN (rating = 'PG-13' AND rental_rate >1 AND rental_rate <= 2) THEN 1
+            ELSE rental_rate
+            END AS promo_rental_rate
+FROM film
+ORDER BY film_id;
+
+
+** VERSION 2 **
+
+WITH pg13_films AS (
+    SELECT film_id, title, rental_rate
+    FROM FILM
+    WHERE rating = 'PG-13'
+)
+
+SELECT 
+    film_id, 
+    title,
+    rental_rate AS current_rental_rate, 
+    CASE 
+        WHEN (film_id IN (SELECT film_id FROM pg13_films) AND f.rental_rate <1) THEN 0.10
+        WHEN (film_id IN (SELECT film_id FROM pg13_films) AND f.rental_rate >1 AND f.rental_rate <= 2) THEN 1
+        ELSE f.rental_rate
+        END AS promo_rental_rate
+FROM film f
+ORDER BY film_id;
+
+** VERSION 3 **
+
+WITH pg13_films AS (
+    SELECT film_id, title, rental_rate
+    FROM FILM
+    WHERE rating = 'PG-13'
+)
+
+SELECT 
+    film_id, 
+    title,
+    rental_rate AS current_rental_rate, 
+    CASE 
+        WHEN (film_id IN (SELECT film_id FROM pg13_films))
+        THEN
+            CASE
+                WHEN (rating = 'PG-13' AND rental_rate <= 1) THEN 0.10
+                WHEN (rating = 'PG-13' AND rental_rate >1 AND rental_rate <= 2) THEN 1
+                ELSE rental_rate
+                END
+        ELSE rental_rate
+        END AS promo_rental_rate
+FROM film f
+ORDER BY film_id;
