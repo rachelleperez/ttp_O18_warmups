635606d93198ecc68e9b250e63df36bccba6a955
Rachelle Perez <rachelleaperez@gmail.com>
2019-10-18 09:51:19 -0400

    updates warmup

--- sql.sql
+++ sql.sql
@@ -34,13 +34,13 @@
 
 *** NOTES***
 
--- Goal - more customers to store. Let's make a promo! 
+-- Goal - more customers to store. Let's make a promo on the PG-13 movies! 
 -- Assignment - generate new prices based on constraints
  --   If movie PG-13 and rent price <= $1 THEN new price 0.10. 
  --   if movie PG-13 and rent price is > 1 AND <= 2 THEN new price 1
  --   else movie PG 13 new price = rent price (current)
 
---Assignment Output: film_id, title, current_rental_rate, new_price
+--Assignment Output: Updated Price list for PG-13 movies >> film_id, title, current_rental_rate, new_price
 
 
 *** QUERY ***
@@ -53,6 +53,7 @@ WITH pg13_films AS (
 
 SELECT 
     film_id, 
+    title,
     rental_rate AS current_rental_rate, 
     CASE 
         WHEN (rental_rate <= 1) THEN 0.10
@@ -64,33 +65,32 @@ ORDER BY film_id;
 
 **** ANSWER (FIRST ROWS ONLY!) ***
 
-film_id | current_rental_rate | promo_rental_rate
----------+---------------------+-------------------
-       7 |                4.99 |              4.99
-       9 |                2.99 |              2.99
-      18 |                0.99 |              0.10
-      28 |                4.99 |              4.99
-      33 |                2.99 |              2.99
-      35 |                2.99 |              2.99
-      36 |                0.99 |              0.10
-      44 |                4.99 |              4.99
-      45 |                4.99 |              4.99
-      48 |                4.99 |              4.99
-      57 |                2.99 |              2.99
-      64 |                0.99 |              0.10
-      67 |                2.99 |              2.99
-      71 |                4.99 |              4.99
-      73 |                2.99 |              2.99
-      79 |                0.99 |              0.10
-      81 |                4.99 |              4.99
-      94 |                2.99 |              2.99
-      96 |                2.99 |              2.99
-      98 |                4.99 |              4.99
-     108 |                0.99 |              0.10
-     124 |                4.99 |              4.99
-     130 |                0.99 |              0.10
-     141 |                4.99 |              4.99
-     152 |                2.99 |              2.99
-     155 |                0.99 |              0.10
-     157 |                0.99 |              0.10
-
+ film_id |            title            | current_rental_rate | promo_rental_rate
+---------+-----------------------------+---------------------+-------------------
+       7 | Airplane Sierra             |                4.99 |              4.99
+       9 | Alabama Devil               |                2.99 |              2.99
+      18 | Alter Victory               |                0.99 |              0.10
+      28 | Anthem Luke                 |                4.99 |              4.99
+      33 | Apollo Teen                 |                2.99 |              2.99
+      35 | Arachnophobia Rollercoaster |                2.99 |              2.99
+      36 | Argonauts Town              |                0.99 |              0.10
+      44 | Attacks Hate                |                4.99 |              4.99
+      45 | Attraction Newton           |                4.99 |              4.99
+      48 | Backlash Undefeated         |                4.99 |              4.99
+      57 | Basic Easy                  |                2.99 |              2.99
+      64 | Beethoven Exorcist          |                0.99 |              0.10
+      67 | Berets Agent                |                2.99 |              2.99
+      71 | Bilko Anonymous             |                4.99 |              4.99
+      73 | Bingo Talented              |                2.99 |              2.99
+      79 | Blade Polish                |                0.99 |              0.10
+      81 | Blindness Gun               |                4.99 |              4.99
+      94 | Braveheart Human            |                2.99 |              2.99
+      96 | Breaking Home               |                2.99 |              2.99
+      98 | Bright Encounters           |                4.99 |              4.99
+     108 | Butch Panther               |                0.99 |              0.10
+     124 | Casper Dragonfly            |                4.99 |              4.99
+     130 | Celebrity Horn              |                0.99 |              0.10
+     141 | Chicago North               |                4.99 |              4.99
+     152 | Circus Youth                |                2.99 |              2.99
+     155 | Cleopatra Devil             |                0.99 |              0.10
+     157 | Clockwork Paradise          |                0.99 |              0.10
